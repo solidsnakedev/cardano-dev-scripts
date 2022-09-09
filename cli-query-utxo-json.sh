01d15beb338b7ff5745674061b4a699429b927b8
SolidSnake <solid@snake>
2022-09-09 14:00:46 -0400

    adding quer-utxo in json format

---
+++ cli-query-utxo-json.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#--------- Import common paths and functions ---------
+source common-utils.sh
+
+#--------- Verification process  ---------
+if [[ "$#" -ne 1 ]]; then error "Missing parameters" && info "Usage: query-utxo <wallet-name>"; exit 1; fi
+# Get wallet name
+wallet_origin=${1}
+
+# Verify if wallet addr exists
+[[ -f ${address_path}/${wallet_origin}.addr ]] || { error "${wallet_origin}.addr missing"; exit 1; }
+
+#--------- Run program ---------
+${cardanocli} query utxo \
+  --testnet-magic $TESTNET_MAGIC \
+  --address $(cat ${address_path}/${wallet_origin}.addr) \
+  --out-file ${datapath}/query-utxo.json
+
+cat ${datapath}/query-utxo.json | jq 
